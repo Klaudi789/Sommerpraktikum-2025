706b0c18ccf1ff4c5e1d7f702fd10a1019d76706
BizNeuron <bruno.zein@gmx.de>
2025-08-07 14:08:18 +0200

    Diagramme

--- Main/public/charts.js
+++
@@ -1,5 +0,0 @@
-import { erstelleLineChartAusDatei } from './LineChart.js';
-import { erstelleBarchartAusDatei } from './BarChart.js';
-
-await erstelleBarchartAusDatei('stackedBarChart', 'line_data.json');
-await erstelleLineChartAusDatei('lineChart', 'line_data.json');
